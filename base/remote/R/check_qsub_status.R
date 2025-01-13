85d0b462415c9318974652cf2c41dabc5b458077
Dongchen Zhang <zhangdc@scc-un4.scc.bu.edu>
2025-01-13 14:23:18 -0500

    Revert changes.

--- base/remote/R/check_qsub_status.R
+++ base/remote/R/check_qsub_status.R
@@ -2,12 +2,11 @@
 #'
 #' @param run run ID, as an integer
 #' @param qstat (string) qstat command for checking job status
-#' @param verbose Boolean: determine if you want to print out the progress, default is TRUE.
 #' @inheritParams remote.execute.cmd
 #'
 #' @return `TRUE` if run is marked as DONE, otherwise FALSE.
 #' @export
-qsub_run_finished <- function(run, host, qstat, verbose = TRUE) {
+qsub_run_finished <- function(run, host, qstat) {
   if (is.na(run)) {
     PEcAn.logger::logger.warn("Job", run, "encountered an error during submission.",
                               "NOTE that the job will be stamped as 'finished' in BETY.")
@@ -17,18 +16,16 @@ qsub_run_finished <- function(run, host, qstat, verbose = TRUE) {
   check <- gsub("@JOBID@", run, qstat)
   if (is.localhost(host)) {
     # Need to use `system` to allow commands with pipes
-    out <- system(check, intern = FALSE, ignore.stdout = TRUE, ignore.stderr = TRUE, wait = TRUE)
+    out <- system(check, intern = TRUE, ignore.stdout = FALSE, ignore.stderr = FALSE, wait = TRUE)
   } else {
     # This uses `system2` under the hood, but that's OK because the entire 
     # command is passed as a single quoted argument, so the pipes are 
     # preserved.
     out <- remote.execute.cmd(host = host, cmd = check, stderr = TRUE)
   }
-
-  if (length(out) > 0 && substring(out, nchar(out) - 3) == "DONE" | out == 1) {
-    if (verbose) {
-      PEcAn.logger::logger.debug("Job", run, "for run", run_id_string, "finished")
-    }
+  
+  if (length(out) > 0 && substring(out, nchar(out) - 3) == "DONE") {
+    PEcAn.logger::logger.debug("Job", run, "for run", run_id_string, "finished")
     return(TRUE)
   } else {
     return(FALSE)
